cccb41a5111cfe572167f8ec8345b7267725df2a
datbui1995 <32448958+datbui1995@users.noreply.github.com>
2020-06-30 00:59:33 +0700

    Initial Commit

---
+++ CustomVideo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CustomVideo
+//
+//  Created by Dat Bui on 6/30/20.
+//  Copyright © 2020 Dat Bui. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
